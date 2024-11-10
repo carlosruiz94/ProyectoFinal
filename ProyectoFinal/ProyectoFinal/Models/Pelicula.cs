ee92217ec4ddd747f6ceaf94a46f05134daead5c
Carlos Ruiz <mastermajora@protonmail.com>
2024-11-09 19:30:33 -0600

    Creación de proyecto final de materia de programación 1

---
+++ ProyectoFinal/ProyectoFinal/Models/Pelicula.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ProyectoFinal.Models
+{
+    public class Pelicula{
+        [Key]
+        public int idPelicula {  get; set; }
+        public string Titulo {  get; set; }
+        public string Autor {  get; set; }
+        public string Sinopsis {  get; set; }
+        public string Duración {  get; set; }
+        public string Clasificación {  get; set; }
+        public byte[] Foto {  get; set; }
+    }
+}
